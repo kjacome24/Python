15d2bfd4d980bbcf3f0d07dbcadd176dc2fc1dac
kjacome24 <kevin.arturo.jacome.duque@gmail.com>
2023-09-19 21:03:41 -0400

    New Changes 19-09-2023

---
+++ fundamentals/oop/Extra_excercises/underscore.py
@@ -0,0 +1,38 @@
+class Underscore:
+    def filter(self, iterable, callback):
+        self.listf = []
+        for i in range(len(iterable)):
+            if callback(iterable[i]):
+                self.listf.append(iterable[i])
+        return self.listf
+    
+    def map(self, iterable, callback):
+        for i in range(len(iterable)):
+            iterable[i] = callback(iterable[i]) 
+        return iterable
+    
+    def find(self, iterable, callback):
+        for i in range(len(iterable)):
+            if callback(iterable[i]):
+                return iterable[i]
+    
+    def reject (self, iterable, callback): 
+        self.listf = []
+        for i in range(len(iterable)):
+            if not callback(iterable[i]):
+                self.listf.append(iterable[i])
+        return self.listf
+
+_ = Underscore()
+
+evens =_.map([1,2,3], lambda x: x*2)
+print(evens)
+
+evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+print(evens)
+
+evens = _.find([1,2,3,4,5,6], lambda x: x>4)
+print(evens)
+
+evens = _.reject([1,2,3,4,5,6], lambda x: x%2==0) # debería devolver [1,3,5]
+print(evens)
