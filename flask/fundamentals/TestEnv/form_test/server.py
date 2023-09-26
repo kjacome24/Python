89ad211b8a60b501b3093bc4df1f91fabd82572d
kjacome24 <kevin.arturo.jacome.duque@gmail.com>
2023-09-26 17:49:18 -0400

    New changes 26/09

---
+++ flask/fundamentals/TestEnv/form_test/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+
+userslist = []
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/users',methods=['POST','GET'])
+def create_user():
+    if request.method == "POST":
+        users = {}
+        print("Got Post Info")
+        users['name'] = request.form.get('name')
+        users['email'] = request.form.get('email')
+        userslist.append(users)
+        # print(request.form)
+        print(userslist)
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route('/added_users')
+def added_users():
+    return render_template("list.html",userslist=userslist)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
--- flask/fundamentals/core_assigments/HTML_Table/server.py
+++ flask/fundamentals/core_assigments/HTML_Table/server.py
@@ -10,6 +10,15 @@ def main():
     {'first_name' : 'KB', 'last_name' : 'Tonel'}]
     return render_template("main.html",users=users)
 
+@app.route('/bootstrap')
+def main2():
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}]
+    return render_template("main_bootstrap.html",users=users)
+
 
 if __name__ == "__main__": 
     app.run(debug=True) 
